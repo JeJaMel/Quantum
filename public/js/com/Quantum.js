5b64dde0b2ddc7298924ef00bc2e95cda36da93e
JeJaMel <jjmelendezb15@gmail.com>
2025-01-28 00:14:18 -0400

    feat: Add QuantumCore to separate the generic methods

--- public/js/com/Quantum.js
+++ public/js/com/Quantum.js
@@ -1,177 +1,35 @@
-export const Quantum = class extends HTMLElement{
-    constructor(props){
-        super()
+import { QuantumCore } from './QuantumCore.js';
+
+export const Quantum = class extends QuantumCore {
+    constructor(props) {
+        super();
         this.attachShadow({ mode: "open" });
-        if(props) this.props = props;
-        this.modules = new Map();
-        this.instances = new Map();
+        if (props) this.props = props;
         this.mapcallbackids = new Map();
-        this._callBack = ()=>{};
-        this.allBuilt = ()=>{};
-        this.svgFiles = new Map();
         this.actZIndex = 0;
-        this.cssFiles = new Map();
-    } 
-    
-    async getCssFile(fileName) {
-        if (!this.cssFiles.has(fileName)) {
+    }
+
+    getMaxZIndex() {
+        this.actZIndex++;
+        return this.actZIndex;
+    }
+
+    parseBoolean(val) {
+        return String(val).toLowerCase() === "true";
+    }
+
+    async createInstance(className, props) {
+        if (!this.instances.has(props.id)) {
             try {
-                let css = await fetch(quantum.routes.css + fileName + ".css").then(response => response.text());
-                this.cssFiles.set(fileName, css);
+                const NewClass = await this.getClass(className);
+                const instance = new NewClass(props);
+                this.addInstance(props.id, instance);
+                return instance;
             } catch (error) {
-                console.error(`Failed to fetch CSS file: ${fileName}`, error);
+                console.error(`Error creando instancia de ${className}:`, error);
                 return null;
             }
         }
-        return this.cssFiles.get(fileName);
+        return this.instances.get(props.id);
     }
-
-    async getSVG(fileName) {
-        if(!this.svgFiles.has(fileName)){
-            try{
-            let svg = await fetch(quantum.routes.icons+fileName+".svg")
-            if(!svg.ok){
-                throw new Error(`Error al cargar SVG: ${fileName} `)
-            }
-
-            let svgText = await svg.text()
-
-            this.svgFiles.set(fileName, svgText);
-            return svgText;
-
-            } catch(err){
-                console.error(`Error in getSVG: ${err.message} `);
-                return null;
-            }
-        } else {
-            return this.svgFiles.get(fileName);
-        }
-    }
-
-    getMaxZIndex(){
-        quantum.actZIndex++;
-    }
-
-    setCallBackBuilt(extFunct){
-        this.callback = extFunct;
-        return this;
-    }
-    
-    getInstance(id){
-        let e = document.getElementById(id);
-        if(e) return e; 
-        else 
-            if(this.instances.has(id)){ return this.instances.get(id) }else {
-            return null
-        }
-    }
-
-    parseBoolean(val){ 
-        return (String(val).toLowerCase() === 'true');
-    }
-
-    async getClass(className){
-        if(!this.modules.has(className)){
-            let clase = await import('./'+className+'.js');
-            this.modules.set(className, clase[className]); 
-        }
-        return  this.modules.get(className);
-    }
-
-    notifyBuilt(id){
-        if(this.mapcallbackids.has(id)){ 
-            this.mapcallbackids.delete(id)  
-            if(this.mapcallbackids.size===0) this._callBack();
-        }
-        return this;
-    }
-
-    addHTMLInstance(obj){
-        if(!this.instances.has(obj.id)){
-            this.instances.set(obj.id, obj);
-        }
-        return this;
-    }
-
-    hide(){this.hidden = true;}
-    show(){this.hidden = false;}
-
-    async createInstance(className, props){
-        try{
-            if(!this.instances.has(props.id)){
-                
-                props.notify = false;
-                let newClass = await this.getClass(className);
-                let i = new newClass(props);
-                this.instances.set(props.id, i);
-                return i;
-            }
-            else{return this.instances.get(props.id)}
-        }
-        catch(e){
-            console.log('No se pudo crear la instancia');
-            return null;
-        }   
-    } 
-    set callback(val){this._callBack = val}
-    get callback() { return this._callBack; } 
-    get callbackids() {return this.mapcallbackids}
-    set callbackids(arr){ for(let id of arr){this.mapcallbackids.set(id,{}); };}
-
-}
-
-    if (!customElements.get ('x-quantum')) {
-        customElements.define ('x-quantum', Quantum);
-    }
-
-    window.Quantum = Quantum;
-    window.quantum = new Quantum();
-    quantum.actZIndex = 0;
-    quantum.radioGroup = []
-    quantum.checkGroup = []
-    quantum.react = () =>{
-        for(let attr in quantum){
-            if(quantum[attr]){
-                if(quantum[attr].value || quantum[attr].equation){
-                    if(quantum[attr]._equation){
-                        quantum[attr].value = eval(quantum[attr].equation.toString());
-                    }
-                }
-            }
-        }
-    }//  que verguero estas haciendo aca hermano naguevona que puta locura nojoda pa vi
-    
-    quantum.routes = {
-        css : '../js/css/',
-        images : '../images/',
-        icons : '../images/icons/'
-    }
-
-    window.Any = class{
-        static observedAttributes = ["equation", "value"];
-        constructor(val){
-            if(val)this._value = val;else this.value=null;
-            this._equation =null;
-        }
-        get value(){return this._value}
-        set value(val){this._value = val; this.react();}
-        get equation(){return this._equation}
-        set equation(val){this._equation = val; this.react();}
-        react(){quantum.react()}
-    }
-    
-    window.Integer = class extends Any{
-        constructor(val){super(val);}
-        parse(val){this._value = val; return parseInt(this._value, 10)}
-    }
-    
-    window.Float = class extends Any{
-        constructor(val){super(val);super.add(this);}
-        parse(val){this._value = val; return parseFloat(this._value)}
-    }
-    
-    addEventListener("DOMContentLoaded", () => {
-        if(quantumInit) quantumInit()
-    });
-
-    
+};
---
+++ public/js/com/QuantumCore.js
@@ -0,0 +1,70 @@
+export const QuantumCore = class {
+    constructor() {
+        this.modules = new Map();
+        this.instances = new Map(); 
+        this.cssFiles = new Map(); 
+        this.svgFiles = new Map(); 
+        this._callBack = () => {}; 
+    }
+
+    async getCssFile(fileName, cssPath = "./css/") {
+        if (!this.cssFiles.has(fileName)) {
+            try {
+                const css = await fetch(`${cssPath}${fileName}.css`).then(response => response.text());
+                this.cssFiles.set(fileName, css);
+            } catch (error) {
+                console.error(`Error cargando el archivo CSS: ${fileName}`, error);
+                return null;
+            }
+        }
+        return this.cssFiles.get(fileName);
+    }
+
+    async getSVG(fileName, svgPath = "./icons/") {
+        if (!this.svgFiles.has(fileName)) {
+            try {
+                const svg = await fetch(`${svgPath}${fileName}.svg`);
+                if (!svg.ok) throw new Error(`Error al cargar SVG: ${fileName}`);
+                const svgText = await svg.text();
+                this.svgFiles.set(fileName, svgText);
+                return svgText;
+            } catch (error) {
+                console.error(`Error en getSVG: ${error.message}`);
+                return null;
+            }
+        }
+        return this.svgFiles.get(fileName);
+    }
+
+    async getClass(className) {
+        if (!this.modules.has(className)) {
+            try {
+                const module = await import(`./${className}.js`);
+                this.modules.set(className, module[className]);
+            } catch (error) {
+                console.error(`Error al importar la clase: ${className}`, error);
+                return null;
+            }
+        }
+        return this.modules.get(className);
+    }
+
+    addInstance(id, instance) {
+        if (!this.instances.has(id)) {
+            this.instances.set(id, instance);
+        }
+        return this;
+    }
+
+    getInstance(id) {
+        return this.instances.get(id) || null;
+    }
+
+    setCallBack(callback) {
+        this._callBack = callback;
+    }
+
+    notifyBuilt() {
+        if (this._callBack) this._callBack();
+    }
+};
