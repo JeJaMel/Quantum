a7fb9cb403e2d3168b200466c6bcf199c6fd3b59
JeJaMel <jjmelendezb15@gmail.com>
2025-02-26 17:46:52 -0400

    fix: Update Quantum.js

--- public/js/com/Quantum.js
+++ public/js/com/Quantum.js
@@ -1,17 +1,17 @@
-export const Quantum = class extends HTMLElement{
-    constructor(props){
+export const Quantum = class extends HTMLElement {
+    constructor(props) {
         super()
-        if(props) this.props = props;
+        if (props) this.props = props;
         this.modules = new Map();
         this.instances = new Map();
         this.mapcallbackids = new Map();
-        this._callBack = ()=>{};
-        this.allBuilt = ()=>{};
+        this._callBack = () => { };
+        this.allBuilt = () => { };
         this.svgFiles = new Map();
         this.actZIndex = 0;
         this.cssFiles = new Map();
-    } 
-    
+    }
+
     async getCssFile(filename) {
         if (!this.cssFiles.has(filename)) {
             try {
@@ -51,125 +51,124 @@ export const Quantum = class extends HTMLElement{
     }
 
 
-    getMaxZIndex(){
+    getMaxZIndex() {
         quantum.actZIndex++;
     }
 
-    setCallBackBuilt(extFunct){
+    setCallBackBuilt(extFunct) {
         this.callback = extFunct;
         return this;
     }
-    
-    getInstance(id){
+
+    getInstance(id) {
         let e = document.getElementById(id);
-        if(e) return e; 
-        else 
-            if(this.instances.has(id)){ return this.instances.get(id) }else {
-            return null
-        }
+        if (e) return e;
+        else
+            if (this.instances.has(id)) { return this.instances.get(id) } else {
+                return null
+            }
     }
 
-    parseBoolean(val){ 
+    parseBoolean(val) {
         return (String(val).toLowerCase() === 'true');
     }
 
-    async getClass(className){
-        if(!this.modules.has(className)){
-            let clase = await import('./'+className+'.js');
-            this.modules.set(className, clase[className]); 
+    async getClass(className) {
+        if (!this.modules.has(className)) {
+            let clase = await import('./' + className + '.js');
+            this.modules.set(className, clase[className]);
         }
-        return  this.modules.get(className);
+        return this.modules.get(className);
     }
 
-    notifyBuilt(id){
-        if(this.mapcallbackids.has(id)){ 
-            this.mapcallbackids.delete(id)  
-            if(this.mapcallbackids.size===0) this._callBack();
+    notifyBuilt(id) {
+        if (this.mapcallbackids.has(id)) {
+            this.mapcallbackids.delete(id)
+            if (this.mapcallbackids.size === 0) this._callBack();
         }
         return this;
     }
 
-    addHTMLInstance(obj){
-        if(!this.instances.has(obj.id)){
+    addHTMLInstance(obj) {
+        if (!this.instances.has(obj.id)) {
             this.instances.set(obj.id, obj);
         }
         return this;
     }
 
-    hide(){this.hidden = true;}
-    show(){this.hidden = false;}
+    hide() { this.hidden = true; }
+    show() { this.hidden = false; }
 
-    async createInstance(className, props){
-        try{
-            if(!this.instances.has(props.id)){
+    async createInstance(className, props) {
+        try {
+            if (!this.instances.has(props.id)) {
                 props.notify = false;
                 let newClass = await this.getClass(className);
                 let i = new newClass(props);
                 this.instances.set(props.id, i);
                 return i;
             }
-            else{return this.instances.get(props.id)}
+            else { return this.instances.get(props.id) }
         }
-        catch(e){
+        catch (e) {
             console.log('No se pudo crear la instancia');
             return null;
-        }   
-    } 
-    
-    set callback(val){this._callBack = val}
-    get callback (){ return this.callback}
-    get callbackids () {return this.mapcallbackids}
-    set callbackids(arr){ for(let id of arr){this.mapcallbackids.set(id,{}); };}
+        }
+    }
+
+    set callback(val) { this._callBack = val }
+    get callback() { return this.callback }
+    get callbackids() { return this.mapcallbackids }
+    set callbackids(arr) { for (let id of arr) { this.mapcallbackids.set(id, {}); }; }
 
 }
 
-    if (!customElements.get ('x-quantum')) {
-        customElements.define ('x-quantum', Quantum);
-    }
+if (!customElements.get('x-quantum')) {
+    customElements.define('x-quantum', Quantum);
+}
 
-    window.Quantum = Quantum;
-    window.quantum = new Quantum();
-    quantum.actZIndex = 0;
-    quantum.radioGroup = []
-    quantum.checkGroup = []
-    quantum.react = () =>{
-        for(let attr in quantum){
-            if(quantum[attr]){
-                if(quantum[attr].value || quantum[attr].equation){
-                    if(quantum[attr]._equation){
-                        quantum[attr].value = eval(quantum[attr].equation.toString());
-                    }
+window.Quantum = Quantum;
+window.quantum = new Quantum();
+quantum.actZIndex = 0;
+quantum.radioGroup = []
+quantum.checkGroup = []
+quantum.react = () => {
+    for (let attr in quantum) {
+        if (quantum[attr]) {
+            if (quantum[attr].value || quantum[attr].equation) {
+                if (quantum[attr]._equation) {
+                    quantum[attr].value = eval(quantum[attr].equation.toString());
                 }
             }
         }
     }
-    
-    quantum.routes = {
-        css : '../js/css/',
-        images : '../images/',
-        icons : '../images/icons/'
-    }
+}
 
-    window.Any = class{
-        static observedAttributes = ["equation", "value"];
-        constructor(val){
-            if(val)this._value = val;else this.value=null;
-            this._equation =null;
-        }
-        get value(){return this._value}
-        set value(val){this._value = val; this.react();}
-        get equation(){return this._equation}
-        set equation(val){this._equation = val; this.react();}
-        react(){quantum.react()}
+quantum.routes = {
+    css: '/Quantum/public/js/css/', //changed
+    images: '/Quantum/public/images/',  //changed
+    icons: '/Quantum/public/images/icons/' //changed
+};
+
+window.Any = class {
+    static observedAttributes = ["equation", "value"];
+    constructor(val) {
+        if (val) this._value = val; else this.value = null;
+        this._equation = null;
     }
-    
-    window.Integer = class extends Any{
-        constructor(val){super(val);}
-        parse(val){this._value = val; return parseInt(this._value, 10)}
-    }
-    
-    window.Float = class extends Any{
-        constructor(val){super(val);super.add(this);}
-        parse(val){this._value = val; return parseFloat(this._value)}
-    }
-    
+    get value() { return this._value }
+    set value(val) { this._value = val; this.react(); }
+    get equation() { return this._equation }
+    set equation(val) { this._equation = val; this.react(); }
+    react() { quantum.react() }
+}
+
+window.Integer = class extends Any {
+    constructor(val) { super(val); }
+    parse(val) { this._value = val; return parseInt(this._value, 10) }
+}
+
+window.Float = class extends Any {
+    constructor(val) { super(val); super.add(this); }
+    parse(val) { this._value = val; return parseFloat(this._value) }
+}
